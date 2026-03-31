044df0cf167f931bd46bb816e7d257abac1841d8
Nicholas <nicholaskuokjinshumg@gmail.com>
2026-03-31 13:58:06 +0800

    added service

--- src/main/kotlin/com/foodorderingsystem/demo/repository/MenuRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.foodorderingsystem.demo.repository
-
-import com.foodorderingsystem.demo.entity.MenuItem
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface MenuRepository : JpaRepository<MenuItem, Long>
